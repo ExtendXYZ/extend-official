5bf6ec9a47dea0f3918904a6916ca8abfca7a767
Andy Yang <andyyang200@gmail.com>
2022-01-24 17:11:56 -0600

    fixes

--- client/src/components/Game/loading_screen.js
+++ client/src/components/Game/loading_screen.js
@@ -17,9 +17,9 @@ export class LoadingScreen extends React.Component {
     return (
 
         <div className="loadingScreen">
-            <img src={require("../../assets/images/space.gif").default} style={{height: window.innerHeight - 300 + "px"}} />
+            <img src={require("../../assets/images/space.gif").default} style={{height: window.innerHeight - 200 + "px"}} />
             <h1> Loading... </h1>
-            <h5> Turn on auto-approving transactions for the best experience </h1>
+            <h5> Turn on auto-approving transactions for the best experience </h5>
 
         </div>
     );
