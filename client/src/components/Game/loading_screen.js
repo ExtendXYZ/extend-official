abe5f54c1bf8bb0a166cc566703550b95746a170
Andy Yang <andyyang200@gmail.com>
2022-01-24 16:59:23 -0600

    improve appearance

--- client/src/components/Game/loading_screen.js
+++ client/src/components/Game/loading_screen.js
@@ -1,21 +1,23 @@
 import React from "react";
-import {View, Image, Text} from "react-native";
 
 
 export class LoadingScreen extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount(){
+    document.body.style.backgroundColor = "black";
+  }
+  componentWillUnmount(){
+    document.body.style.backgroundColor = null;
+  }
+
   render() {
     return (
 
-        <div className="loadingScreen" style=
-          {{
-            display: "flex",
-            backgroundColor: "black",
-          }}
-        >
-            <img src={require("../../assets/images/space.gif").default} style={{height: window.innerHeight - 63 + "px"}} />
+        <div className="loadingScreen">
+            <img src={require("../../assets/images/space.gif").default} style={{height: window.innerHeight - 300 + "px"}} />
             <h1> Loading... </h1>
             <h1> Turn on auto-approve transactions for best experience </h1>
 
