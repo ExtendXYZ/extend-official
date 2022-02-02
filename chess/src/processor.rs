7b913e23cfc86cc71d3111bae6dd466c45321850
Zhixian Lei <leizhixian.research@gmail.com>
2022-02-02 12:14:20 -0500

    chess skeleton

---
+++ chess/src/processor.rs
@@ -0,0 +1,18 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+pub struct Processor;
+impl Processor {
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let (tag, rest) = instruction_data
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+        Ok(())
+    }
+}
