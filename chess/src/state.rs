6fa0ad45014137fdd9def3a6b0eb45b0c09c7ce4
Zhixian Lei <leizhixian.research@gmail.com>
2022-02-03 16:15:22 -0500

    states and instructions to start

--- chess/src/state.rs
+++ chess/src/state.rs
@@ -18,30 +18,16 @@ impl Base {
 
 // begin color program state
 pub const NEIGHBORHOOD_BOARD_BASE_SEED: &[u8] = b"neighborhood_board_base";
-pub const NEIGHBORHOOD_BOARD_POINTER_SEED: &[u8] = b"neighborhood_board_pointer";
-pub const MAX_BOARDS: u64 = 6;
 
 pub const NEIGHBORHOOD_BOARD_BASE_RESERVE: usize = 256;
 #[repr(C)]
 #[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
 pub struct NeighborhoodBoardBase {
     pub bump: u8,
-    pub length: u64,
-}
-
-impl NeighborhoodBoardBase {
-    pub const LEN: usize = size_of::<u8>() + size_of::<u64>();
-}
-
-pub const NEIGHBORHOOD_BOARD_POINTER_RESERVE: usize = 128;
-#[repr(C)]
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
-pub struct NeighborhoodBoardPointer {
-    pub bump: u8,
     pub boardkey: Pubkey,
 }
 
-impl NeighborhoodBoardPointer {
+impl NeighborhoodBoardBase {
     pub const LEN: usize = size_of::<u8>() + size_of::<Pubkey>();
 }
 
@@ -52,7 +38,19 @@ pub enum Phase {
     BlackTurn,
     WhiteWin,
     BlackWin,
-    Draw
+    Draw,
+    Resign,
+}
+
+pub enum Whitelist {
+    Neighborhood,
+    Pubkey,
+}
+
+pub enum Side {
+    Undefined,
+    White,
+    Black,
 }
 
 pub const BOARD_RESERVE: usize = 130000;
@@ -61,10 +59,14 @@ pub const BOARD_RESERVE: usize = 130000;
 pub struct Board {
     pub votes: [[u8; 3]; NEIGHBORHOOD_SIZE * NEIGHBORHOOD_SIZE],
     pub game_arr: [u8; 73],
-    pub white_whitelist: [[u8; 2]; 10],
-    pub black_whitelist: [[u8; 2]; 10],
-    pub max_white_player: u16,
-    pub max_black_player: u16,
+    pub white_pubkey: Pubkey,
+    pub black_pubkey: Pubkey,
+    pub white_type: u8,
+    pub black_type: u8,
+    // pub white_whitelist: [[u8; 2]; 10],
+    // pub black_whitelist: [[u8; 2]; 10],
+    // pub max_white_player: u16,
+    // pub max_black_player: u16,
     pub min_white_vote: u16,
     pub min_black_vote: u16,
     pub num_white_vote: u16,
@@ -73,10 +75,9 @@ pub struct Board {
     pub max_timeout_vote: u64,
     pub phase_start: u64,
     pub phase: u8,
-    pub step: u8,
-    
+    pub num_move: u16,
 }
 
 impl Board {
-    pub const LEN: usize = 3 * NEIGHBORHOOD_SIZE * NEIGHBORHOOD_SIZE + 73 + 20 * 2 + 2 * 8 + 8 * 3 + 1 + 1;
+    pub const LEN: usize = 3 * NEIGHBORHOOD_SIZE * NEIGHBORHOOD_SIZE + 73 + 2 * 32 + 2 * 4 + 4 * 4 + 3 * 8 + 1 + 2;
 }
