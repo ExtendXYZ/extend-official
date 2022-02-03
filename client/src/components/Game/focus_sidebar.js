a46037cda0b948491399905002c25bf248488c85
Andy Yang <andyyang200@gmail.com>
2022-02-03 15:36:19 -0600

    error handling in refreshing unregistered spaces

--- client/src/components/Game/focus_sidebar.js
+++ client/src/components/Game/focus_sidebar.js
@@ -531,7 +531,7 @@ export class FocusSidebar extends React.Component {
                                     size="small"
                                     variant="contained"
                                     onClick={() => {
-                                        this.props.handleFocusRefresh();
+                                        this.props.focusRefresh();
                                     }}
                                     style={{
                                         width: "100%",
