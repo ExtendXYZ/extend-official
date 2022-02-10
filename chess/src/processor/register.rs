eb6bcb1dfe5b1b39613fa510b6e6ccc711ac9844
OccasionalDegen <occasionaldegen@gmail.com>
2022-02-10 13:59:23 -0500

    Completes fool's mate sequence

--- chess/src/processor/register.rs
+++ chess/src/processor/register.rs
@@ -42,7 +42,8 @@ pub fn process(
     // Board is initialized
     msg!("r space {:?}", args.space);
     msg!("r side {:?}", args.side);
-    let board_state: Board = try_from_slice_unchecked(&board_account.data.borrow())?;
+    let mut board_state: Board = try_from_slice_unchecked(&board_account.data.borrow())?;
+    let board_data = &mut *board_account.data.borrow_mut();
 
     // Board is currently registering
     if board_state.phase != Phase::Registering {
@@ -52,10 +53,25 @@ pub fn process(
     // Check if deadline has passed, short-circuit if so
     let now_ts = Clock::get().unwrap().unix_timestamp as u64;
     if board_state.next_deadline < now_ts {
-        // Advance phase if conditions met
-        // Extend deadline if not
-        // TODO
-        return Ok(());
+        msg!("Deadline hit, checking whether to advance phase");
+        let mut advance = false;
+        if board_state.player_white.has_pk && board_state.player_black.has_pk {
+            advance = true;
+        } else {
+            // Quorum met where applicable
+            // TODO
+        }
+        if advance {
+            msg!("Advance phase to active");
+            board_state.phase = Phase::Active;
+            board_state.next_deadline = now_ts + board_state.interval_move;
+            board_state.serialize(board_data)?;
+            return Ok(());
+        } else {
+            msg!("Conditions not met; extending");
+            board_state.next_deadline = now_ts + board_state.interval_register;
+            board_state.serialize(board_data)?;
+        }
     }
 
     // Account owns space
@@ -69,7 +85,7 @@ pub fn process(
 
     // Assign space
     let space_index = Board::LEN + get_index(args.space.x, args.space.y);
-    let space_data = &mut (&mut board_account.data.borrow_mut()[space_index..space_index+1]);
+    let space_data = &mut (&mut board_data[space_index..space_index+1]);
     args.side.serialize(space_data)?;
 
     Ok(())
