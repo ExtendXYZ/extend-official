d00567dfc5015ffa09bb1851f30a4f4976f3782b
Edward Xu <ex10.2k12@gmail.com>
2022-02-15 11:02:14 -0600

    update rpc token

--- cli/src/register.sh
+++ cli/src/register.sh
@@ -1,12 +1,13 @@
 set -e
 
-while getopts k:e: flag
+while getopts k:r:e: flag
 do
     case "${flag}" in
         k) KEYPAIR=${OPTARG};;
+        r) R=${OPTARG};;
         e) ENV=${OPTARG};;
     esac
 done
 
 echo $KEYPAIR
-npx ts-node extend-cli.ts register -k ${KEYPAIR} -l trace -e ${ENV}
+npx ts-node extend-cli.ts register -k ${KEYPAIR} -l trace -r ${R} -e ${ENV}
