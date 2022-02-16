3ecd0d0381a561ffa803217ee58456537abc4eb3
Anirudh Suresh <anirudhtx@gmail.com>
2022-02-16 10:37:36 -0600

    readd mktplace (#169)
    * add back listing stats to nbd sidebar
    
    * add back mktplace to selecting sidebar
    
    * add back mktplace to focus sidebar

--- client/src/components/Game/focus_sidebar.js
+++ client/src/components/Game/focus_sidebar.js
@@ -393,11 +393,8 @@ export class FocusSidebar extends React.Component {
                         {sidebarHeader}
 
                         {/* purchase info */}
-                        {<Divider className="sidebarDivider">
-                            Marketplace Coming Soon!
-                        </Divider>}
                         
-                        {/* {(!this.props.focus.infoLoaded || !this.props.focus.imgLoaded) ?
+                        {(!this.props.focus.infoLoaded || !this.props.focus.imgLoaded) ?
                             null
                             :
                             <>
@@ -516,7 +513,7 @@ export class FocusSidebar extends React.Component {
                                 </>
                             ) : null}
                             </>
-                        } */}
+                        }
                     </TabPanel>
 
                     <TabPanel value={this.state.value} index={2}>
